a1f327361d3fcf7e37fece20f506cb3c1c373bc6
Alex <t_tapak@yahoo.com>
2016-08-01 22:16:01 +0300

    Initial commit

---
+++ src/converter.js
@@ -0,0 +1,96 @@
+/**
+ *
+ * @package     Converter
+ * @version     1.0
+ * @license     http://mit-license.org/
+ * @author      Tapakan https://github.com/Tapakan
+ * @coder       Alexander Oganov <t_tapak@yahoo.com>
+ */
+
+Converter = window.Converter || {};
+
+(function (Converter, document) {
+    "use strict";
+
+    /**
+     * Init with currencies params.
+     * @param settings
+     */
+    Converter.init = function (settings) {
+        if (typeof settings != 'object') {
+            throw new Error('Settings must be an instance of object.');
+        }
+
+        this.settings = settings;
+    };
+
+    /**
+     * Returns symbol for currency.
+     * @param currency
+     * @returns {string|string|string|*}
+     */
+    Converter.getSymbol = function (currency) {
+        if (!_checkCurrency(currency)) {
+            throw new Error("Currency" + currency + " does\'t exists.");
+        }
+
+        return this.settings[currency].symbol;
+    };
+
+    /**
+     * Returns rate for currency.
+     * @param currency
+     * @returns {number}
+     */
+    Converter.getRate = function (currency) {
+        if (!_checkCurrency(currency)) {
+            throw new Error("Currency" + currency + " does\'t exists.");
+        }
+
+        return this.settings[currency].rate;
+    };
+
+    /**
+     * Convert value from currency to currency.
+     * Before converting check currency.
+     * @param {string} from
+     * @param {string} to
+     * @param {float|int} value
+     */
+    Converter.value = function (from, to, value) {
+        if (!_checkCurrency(from) || !_checkCurrency(to)) {
+            throw new Error("Invalid currency.");
+        }
+
+        return _convert(from, to, value);
+    };
+
+    /**
+     * Check currency for existing.
+     * @param {string} currency
+     * @returns {boolean}
+     * @private
+     */
+    function _checkCurrency(currency) {
+        return Converter.settings.hasOwnProperty(currency);
+    }
+
+    /**
+     * Converting value from one currency to other currency.
+     * @param {string} from
+     * @param {string} to
+     * @param {float|int} value
+     * @private
+     */
+    function _convert(from, to, value) {
+        var newValue = value;
+        if (from != to) {
+            newValue = value / Converter.settings[from].rate;
+            newValue = newValue * Converter.settings[to].rate;
+        }
+
+        return newValue;
+    }
+
+    Converter.init(currencies);
+})(Converter, document);
---
+++ tests/converter-tests.js
@@ -0,0 +1,28 @@
+describe("Converter", function () {
+
+    describe("Get symbol", function () {
+        it("Symbol of UAH is UAH", function () {
+            assert.equal(Converter.getSymbol("UAH"), "UAH");
+        });
+        it("Symbol of EUR is €", function () {
+            assert.equal(Converter.getSymbol("EUR"), "€");
+        });
+        it("Symbol of USD is $", function () {
+            assert.equal(Converter.getSymbol("USD"), "$");
+        });
+    });
+
+    describe("Convert", function() {
+        it("from UAH to EUR. 2 EUR = 60 UAH", function () {
+            assert.equal(Converter.value("EUR", "UAH", 2), 60);
+        });
+
+        it("from USD to EUR. 2 EUR = 3 USD", function () {
+            assert.equal(Converter.value("EUR", "USD", 2), 3);
+        });
+
+        it("from EUR to EUR. 2 EUR = 2 EUR", function () {
+            assert.equal(Converter.value("EUR", "EUR", 2), 2);
+        });
+    });
+});
