9f33c976180a36c09ec5a5ebdeca94f055b308cb
Alex <t_tapak@yahoo.com>
2016-08-07 15:00:08 +0300

    Update converter

--- tests/converter-tests.js
+++ tests/converter-tests.js
@@ -1,5 +1,3 @@
-var assert     = require('assert');
-var Converter  = require('../src/converter.js');
 
 var currencies = {
     "UAH": {
@@ -16,33 +14,33 @@ var currencies = {
     }
 };
 
-GLOBAL.Converter.init(currencies);
+Converter.init(currencies);
 
 describe("Converter", function () {
 
     describe("Get symbol", function () {
         it("Symbol of UAH is UAH", function () {
-            assert.equal(GLOBAL.Converter.getSymbol("UAH"), "UAH");
+            assert.equal(Converter.getSymbol("UAH"), "UAH");
         });
         it("Symbol of EUR is €", function () {
-            assert.equal(GLOBAL.Converter.getSymbol("EUR"), "€");
+            assert.equal(Converter.getSymbol("EUR"), "€");
         });
         it("Symbol of USD is $", function () {
-            assert.equal(GLOBAL.Converter.getSymbol("USD"), "$");
+            assert.equal(Converter.getSymbol("USD"), "$");
         });
     });
 
     describe("Convert", function () {
         it("from UAH to EUR. 2 EUR = 60 UAH", function () {
-            assert.equal(GLOBAL.Converter.value("EUR", "UAH", 2), 60);
+            assert.equal(Converter.value("EUR", "UAH", 2), 60);
         });
 
         it("from USD to EUR. 2 EUR = 3 USD", function () {
-            assert.equal(GLOBAL.Converter.value("EUR", "USD", 2), 3);
+            assert.equal(Converter.value("EUR", "USD", 2), 3);
         });
 
         it("from EUR to EUR. 2 EUR = 2 EUR", function () {
-            assert.equal(GLOBAL.Converter.value("EUR", "EUR", 2), 2);
+            assert.equal(Converter.value("EUR", "EUR", 2), 2);
         });
     });
 });
