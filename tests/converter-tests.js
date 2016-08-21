ef63058793dd7f9b8f031964dc945931bb7d69f8
Alex <t_tapak@yahoo.com>
2016-08-21 15:42:50 +0300

    News tests for Error

--- tests/converter-tests.js
+++ tests/converter-tests.js
@@ -1,60 +1,75 @@
-
-
 (function () {
-  "use strict";
+    "use strict";
 
-  var currencies = {
-    "UAH": {
-      "rate"  : 30,
-      "symbol": "UAH"
-    },
-    "EUR": {
-      "rate"  : 1,
-      "symbol": "€"
-    },
-    "USD": {
-      "rate"  : 1.5,
-      "symbol": "$"
-    }
-  };
+    var currencies = {
+        "UAH": {
+            "rate"  : 30,
+            "symbol": "UAH"
+        },
+        "EUR": {
+            "rate"  : 1,
+            "symbol": "€"
+        },
+        "USD": {
+            "rate"  : 1.5,
+            "symbol": "$"
+        }
+    };
 
-  Converter.init(currencies);
+    Converter.init(currencies);
 
-  describe("Converter", function () {
+    describe("Converter", function () {
 
-    describe("Get symbol", function () {
-      it("Symbol of UAH is UAH", function () {
-        expect("UAH").toBe(Converter.getSymbol("UAH"));
-      });
-      it("Symbol of EUR is €", function () {
-        expect("€").toBe(Converter.getSymbol("EUR"));
-      });
-      it("Symbol of USD is $", function () {
-        expect("$").toBe(Converter.getSymbol("USD"));
-      });
+        describe("Get symbol", function () {
+            it("Symbol of UAH is UAH", function () {
+                expect("UAH").toBe(Converter.getSymbol("UAH"));
+            });
+            it("Symbol of EUR is €", function () {
+                expect("€").toBe(Converter.getSymbol("EUR"));
+            });
+            it("Symbol of USD is $", function () {
+                expect("$").toBe(Converter.getSymbol("USD"));
+            });
+
+            it("Not found currency. Expected Error", function() {
+                expect(function() {
+                    Converter.getSymbol("BYR");
+                }).toThrow();
+            });
+        });
+
+        describe("Convert", function () {
+            it("from UAH to EUR. 2 EUR = 60 UAH", function () {
+                expect(60).toEqual(Converter.value("EUR", "UAH", 2));
+            });
+
+            it("from USD to EUR. 2 EUR = 3 USD", function () {
+                expect(3).toEqual(Converter.value("EUR", "USD", 2));
+            });
+
+            it("from EUR to EUR. 2 EUR = 2 EUR", function () {
+                expect(2).toEqual(Converter.value("EUR", "EUR", 2));
+            });
+
+            it("Not found currency. Expected Error", function() {
+                expect(function() {
+                    Converter.value("BYR", "EUR", 40);
+                }).toThrow();
+            });
+        });
+
+        describe("getRate", function () {
+            it("rate of UAH is 30", function () {
+                expect(30).toEqual(Converter.getRate("UAH"));
+            });
+
+            it("Not found currency. Expected Error", function() {
+                expect(function() {
+                    Converter.getRate("BYR");
+                }).toThrow();
+            });
+        });
+
     });
-
-    describe("Convert", function () {
-      it("from UAH to EUR. 2 EUR = 60 UAH", function () {
-        expect(60).toEqual(Converter.value("EUR", "UAH", 2));
-      });
-
-      it("from USD to EUR. 2 EUR = 3 USD", function () {
-        expect(3).toEqual(Converter.value("EUR", "USD", 2));
-      });
-
-      it("from EUR to EUR. 2 EUR = 2 EUR", function () {
-        expect(2).toEqual(Converter.value("EUR", "EUR", 2));
-      });
-    });
-
-    describe("getRate", function () {
-      it("rate of UAH is 30", function () {
-        expect(30).toEqual(Converter.getRate("UAH"));
-      });
-
-    });
-
-  });
 
 }).call(this);
